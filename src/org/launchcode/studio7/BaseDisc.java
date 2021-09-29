97fc7d8c9a1659ef8d08745469173600b98cd86e
Awol-E <endriesawol@gmail.com>
2021-09-28 21:06:13 -0500

    completed Studio 8

---
+++ src/org/launchcode/studio7/BaseDisc.java
@@ -0,0 +1,54 @@
+package org.launchcode.studio7;
+
+import java.util.HashMap;
+
+public abstract class BaseDisc {
+    // class variable
+    private String discData;
+    private HashMap<String, String> discInfo;
+    private boolean isInserted;
+
+    private final int storageCapacityInMB;
+    private final int spinSpeedInRAM;
+
+    // Constructor
+    public BaseDisc(int storageCapacity, int spinSpeed){
+        this.discData = "";
+        this.discInfo = new HashMap<String, String>();
+        this.isInserted = false;
+
+        this.storageCapacityInMB = storageCapacity;
+        this.spinSpeedInRAM = spinSpeed;
+
+        this.discInfo.put("Storage Capacity", String.valueOf(storageCapacity));
+        this.discInfo.put("Spin Speed", String.valueOf(spinSpeed));
+    }
+    public BaseDisc(int storageCapacity, int spinSpeed, String title){
+        this(storageCapacity, spinSpeed);
+        this.discInfo.put("Title", title);
+    }
+
+    //methods
+    // Getters & Setters
+    public boolean isInserted() { return this.isInserted; }
+
+    public int getStorageCapacity() { return this.storageCapacityInMB; }
+
+    public int getSpinSpeed() { return this.spinSpeedInRAM; }
+
+    public void writeData(String data) {
+        this.discData += data;
+    }
+
+    public String readData(){
+        return this.discData;
+    }
+
+    public HashMap<String, String> getInfo(){
+        return this.discInfo;
+    }
+
+    public void insert(){
+        this.isInserted = true;
+    }
+}
